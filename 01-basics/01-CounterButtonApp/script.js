b6cab016dd0e5cad6f16ebada9163d1ff9423804
ViktoriyaHor <vhoroshun.face@gmail.com>
2021-08-05 19:00:29 +0300

    01-CounterButtonApp

--- 01-basics/01-CounterButtonApp/script.js
+++ 01-basics/01-CounterButtonApp/script.js
@@ -1,3 +1,25 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp, defineComponent } from './vendor/vue.esm-browser.js';
 
 // Создайте Vue приложение
+
+const RootComponentOptions = defineComponent({
+  name: 'Root',
+
+  data() {
+    return {
+      value: 0,
+    };
+  },
+
+  methods: {
+    incrementValue: function() {
+      this.value += 1;
+    },
+  },
+
+  template: '<button @click="incrementValue" type="button">{{ value }}</button>',
+});
+
+const app = createApp(RootComponentOptions);
+
+const vm = app.mount('#app');
