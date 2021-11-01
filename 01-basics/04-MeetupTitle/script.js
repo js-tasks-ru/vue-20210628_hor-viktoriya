7e3477c64376be3b22416ababa911ddf72b91a7b
ViktoriyaHor <vhoroshun.face@gmail.com>
2021-11-01 12:27:12 +0200

    04-MeetupTitle

--- 01-basics/04-MeetupTitle/script.js
+++ 01-basics/04-MeetupTitle/script.js
@@ -1,17 +1,44 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp, defineComponent } from './vendor/vue.esm-browser.js';
 
 const API_URL = 'https://course-vue.javascript.ru/api';
 
-function fetchMeetupById(meetupId) {
-  return fetch(`${API_URL}/meetups/${meetupId}`).then((response) => {
-    if (response.ok) {
-      return response.json();
-    } else {
-      return response.json().then((error) => {
-        throw error;
-      });
-    }
-  });
-}
-
 // Требуется создать Vue приложение
+
+const RootAppComponent = defineComponent({
+  name: 'Root',
+
+  data() {
+    return {
+      meetup: null,
+      meetupId: '',
+    };
+  },
+
+  watch: {
+    meetupId: {
+      handler() {
+        this.fetchMeetupById();
+      },
+    },
+  },
+
+  methods: {
+    fetchMeetupById() {
+      return fetch(`${API_URL}/meetups/${this.meetupId}`).then((response) => {
+        if (response.ok) {
+          response.json().then((data) => {
+            this.meetup = data;
+          });
+        } else {
+          return response.json().then((error) => {
+            throw error;
+          });
+        }
+      });
+    },
+  },
+});
+
+const app = createApp(RootAppComponent);
+const vm = app.mount('#app');
+window.vm = vm;
